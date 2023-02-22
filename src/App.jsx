d99bd006914d53a737a3c80216033a6ba4d88db3
shengpingyao <13221050207@163.com>
2023-02-22 17:27:20 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './style.scss';
+class App extends Component {
+    render(){
+        return (
+            <div className="box">
+                hello,React
+                <span>12344</span>
+            </div>
+        )
+    }
+}
+export default App;
